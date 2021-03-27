c9303c3bd74b760ceff6f82a6a5089d24ebff42c
Roberto Hernandez <00162317@uca.edu.sv>
2021-03-27 02:57:15 -0600

    Muestra nombre, url imagen y genero en formato lista por el momento

--- js/script.js
+++ js/script.js
@@ -1,26 +1,56 @@
-document.getElementById('button3').addEventListener('click', getDataFromExternalAPI);
+//document.getElementById('button3').addEventListener('click', getDataFromExternalAPI);
+document.getElementById('button2').addEventListener('click', getSearchData);
+var valor = document.getElementById('buscar_s');
+//document.getElementById('btn_buscar').addEventListener('click', getSearchData);
 
-function getDataFromExternalAPI() {
-    fetch('https://www.superheroapi.com/api.php/3945365972188670/70')
+function getData() {
+    console.log("------")
+}
+
+function getSearchData() {
+    alert(valor.value);
+    //valor='batman';
+    fetch('https://www.superheroapi.com/api.php/3945365972188670/search/' + valor.value)
         .then(res => res.json())
         .then(data => {
-          console.log(data.name);
-          var texto = data.name;
-          document.getElementById('texto').innerHTML=texto;
-  
-          // Displaying to the UI
-          let output = '';
-        
-          /*data.forEach(function(user) {
-            output +=`<li>${user.power}</li>`
-          });*/
-          document.getElementById('output').innerHTML = output;
+            console.log(data.results);
+            
+            data.results.forEach(e=>$('#test')
+            .append('<li>'+e.name+ '</li>' + "<br>"+
+            '<li>'+e.appearance.gender+ '</li>' + "<br>"+
+            '<li>'+ '<img>' +e.image.url+ '</img>' + '</li>' + "<br>"));
+
         })
         .catch(err => {
-          console.log(err);
-  
-          // Displaying to the UI
-          document.getElementById('output').innerHTML = err;
+            console.log(err);
+
+            // Displaying to the UI
+            document.getElementById('output2').innerHTML = err;
         });
-  
-  }
+}
+
+/*
+function getDataFromExternalAPI() {
+    fetch('https://www.superheroapi.com/api.php/3945365972188670/' + '70')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.name);
+            var texto = data.name;
+            document.getElementById('texto').innerHTML = texto;
+
+            // Displaying to the UI
+            let output = '';
+
+            data.forEach(function(user) {
+              output +=`<li>${user.power}</li>`
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch(err => {
+            console.log(err);
+
+            // Displaying to the UI
+            document.getElementById('output').innerHTML = err;
+        });
+
+}*/
