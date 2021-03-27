977563bfcb86be585edfb95833e297a4796ddbe2
Roberto Hernandez <00162317@uca.edu.sv>
2021-03-27 03:54:07 -0600

    Trabajando poco de diseño y agregando futuro htmls a utilizar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-//document.getElementById('button3').addEventListener('click', getDataFromExternalAPI);
 document.getElementById('button2').addEventListener('click', getSearchData);
 var valor = document.getElementById('buscar_s');
-//document.getElementById('btn_buscar').addEventListener('click', getSearchData);
 
 function getData() {
     console.log("------")
@@ -28,29 +26,3 @@ function getSearchData() {
             document.getElementById('output2').innerHTML = err;
         });
 }
-
-/*
-function getDataFromExternalAPI() {
-    fetch('https://www.superheroapi.com/api.php/3945365972188670/' + '70')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.name);
-            var texto = data.name;
-            document.getElementById('texto').innerHTML = texto;
-
-            // Displaying to the UI
-            let output = '';
-
-            data.forEach(function(user) {
-              output +=`<li>${user.power}</li>`
-            });
-            document.getElementById('output').innerHTML = output;
-        })
-        .catch(err => {
-            console.log(err);
-
-            // Displaying to the UI
-            document.getElementById('output').innerHTML = err;
-        });
-
-}*/
